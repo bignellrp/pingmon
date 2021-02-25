e4fb42e8adc192a49c3d541b170542366ada1472
Richard Bignell <bignellrp@users.noreply.github.com>
2021-02-25 19:53:52 +0000

    Update pingmon.sh

--- pingmon.sh
+++ pingmon.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 WEBHOOK='https://hooks.slack.com/services/YOURWEBHOOKCODEHERE'
-NODES='pingmon_nodes'
-LOG='/var/log/pingmon.log'
+NODES='/boot/config/plugins/user.scripts/scripts/pingmon/pingmon_nodes'
+LOG='/var/log/syslog'
+DATEFORMAT=`date '+%b %d %T'`
 
 while read LINE; do
     NAME=$(echo $LINE|awk -F "," '{print ($1)}')
     IP=$(echo $LINE|awk -F "," '{print ($2)}')
     if ping -c 3 $IP &> /dev/null; then
-        echo "$(date -Iseconds), $NAME, Ping is ok!" >> $LOG
+        echo "$DATEFORMAT $HOSTNAME pingmon: $NAME Ping is ok!" >> $LOG
     else
         curl -X POST -H 'Content-type: application/json' --data '{"text": "'"${NAME} is down!"'"}' $WEBHOOK
-        echo "$(date -Iseconds), $NAME, Ping is down!" >> $LOG
+        echo "$DATEFORMAT $HOSTNAME pingmon: $NAME Ping is down!" >> $LOG
     fi
 done < $NODES
